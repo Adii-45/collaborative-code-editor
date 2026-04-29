20603ed610be1669c10b64b345052160232c6a97
Aditya Kr. Nayak <adi.nayak45@gmail.com>
2026-04-29 15:00:55 +0530

    refactor: remove unused index files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import Room from './pages/Room'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Room from './pages/Room';
 
 function App() {
   return (
-    <div className="w-screen h-screen flex flex-col bg-[#0d1117] text-white">
-      <Room />
-    </div>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="w-screen h-screen flex flex-col bg-[#0d1117] text-white">
+          <Routes>
+            {/* Public routes */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+
+            {/* Protected routes */}
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/editor/:projectId"
+              element={
+                <ProtectedRoute>
+                  <Room />
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Default redirect */}
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="*" element={<Navigate to="/dashboard" replace />} />
+          </Routes>
+        </div>
+
+        {/* Toast notifications */}
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            style: {
+              background: '#161b22',
+              color: '#e6edf3',
+              border: '1px solid #30363d',
+              fontSize: '14px',
+            },
+          }}
+        />
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
